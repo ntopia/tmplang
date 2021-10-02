5720f17d8f68bc208e74c7d41bd8848f20a6d519
ntopia <ntopia.p@gmail.com>
2021-10-02 08:04:10 +0000

    Hindley–Milner type inference algorithm WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "antlr4-runtime.h"
 #include "TmplangLexer.h"
@@ -7,12 +8,89 @@
 
 using namespace antlr4;
 
-class TreeListener : public TmplangBaseListener {
-public:
+int typecounter = 0;
+enum TypeKind {
+  INT,
+  CHAR,
+  FUNC,
+  TYPE_VAR,
+};
+struct Type {
+  TypeKind type;
+  int id;
+  std::vector<Type> from;
+  int to;
+};
+
+struct TypeEquation {
+  Type left, right;
+};
+
+class AssignTypenameListener : public TmplangBaseListener {
+ private:
+  tree::ParseTreeProperty<Type> props;
+  std::vector<TypeEquation> equations;
+
+ public:
   void exitFile(TmplangParser::FileContext *ctx) override {
     std::cout << "parsed function list\n";
     for (auto *fn : ctx->function()) {
       std::cout << fn->ID()->toString() << std::endl;
+    }
+  }
+
+  void enterFunctionCallExpr(TmplangParser::FunctionCallExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void exitFunctionCallExpr(TmplangParser::FunctionCallExprContext *ctx) override {
+    std::vector<Type> childTypes;
+    for (auto* expr : ctx->exprList()->expr()) {
+      childTypes.push_back(props.get(expr));
+    }
+    //equations.push_back(TypeEquation{ ctx-> props.get(ctx), Type{ FUNC, typecounter++, childTypes, }})
+  }
+
+  void enterNegateExpr(TmplangParser::NegateExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterNotExpr(TmplangParser::NotExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterMulDivExpr(TmplangParser::MulDivExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterPlusMinusExpr(TmplangParser::PlusMinusExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterEqualExpr(TmplangParser::EqualExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterVarRefExpr(TmplangParser::VarRefExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterLiteralExpr(TmplangParser::LiteralExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterParenExpr(TmplangParser::ParenExprContext *ctx) override {
+    props.put(ctx, Type{ TYPE_VAR, typecounter++ });
+  }
+
+  void enterLiteral(TmplangParser::LiteralContext *ctx) override {
+    if (ctx->CharacterLiteral() != nullptr) {
+      std::cout << "character literal\n";
+      props.put(ctx, Type{ CHAR, typecounter++ });
+    }
+    else if (ctx->IntegerLiteral() != nullptr) {
+      std::cout << "integer literal " << ctx->IntegerLiteral()->toString() << std::endl;
+      props.put(ctx, Type{ INT, typecounter++ });
     }
   }
 };
@@ -24,7 +102,7 @@ int main(int argc, const char *argv[]) {
   TmplangParser parser(&tokens);
 
   tree::ParseTree *tree = parser.file();
-  TreeListener listener;
+  AssignTypenameListener listener;
   tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
 
   return 0;
